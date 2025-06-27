9e78986c63eec1fd3cb7132e08d7bc8dac48ae99
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-06-27 19:45:03 -0300

    musica model and db file added

---
+++ models/musica.py
@@ -0,0 +1,44 @@
+import uuid
+from models.db import db
+
+class Musica(db.Model):
+    __tablename__ = 'musica'
+
+    id_cancion = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    cancion = db.Column(db.String(100))
+    artista = db.Column(db.String(100))
+    album = db.Column(db.String(100))
+    anio = db.Column(db.Integer)
+    duracion = db.Column(db.Integer) # Duracion de la cancion en segundos
+    fecha_lanzamiento = db.Column(db.Date)
+    hora_estreno = db.Column(db.Time)
+    descripcion = db.Column(db.Text)
+    email_contacto = db.Column(db.String(100))
+    activo = db.Column(db.Boolean, default=True)
+
+    def __init__(self, cancion, artista, album, anio, duracion, fecha_lanzamiento, hora_estreno, descripcion, email_contacto, activo=True):
+        self.cancion = cancion
+        self.artista = artista
+        self.album = album
+        self.anio = anio
+        self.duracion = duracion
+        self.fecha_lanzamiento = fecha_lanzamiento
+        self.hora_estreno = hora_estreno
+        self.descripcion = descripcion
+        self.email_contacto = email_contacto
+        self.activo = activo
+
+    def serialize(self):
+        return {
+            'id_cancion': self.id_cancion,
+            'cancion': self.cancion,
+            'artista': self.artista,
+            'album': self.album,
+            'anio': self.anio,
+            'duracion': self.duracion,
+            'fecha_lanzamiento': self.fecha_lanzamiento,
+            'hora_estreno': self.hora_estreno,
+            'descripcion': self.descripcion,
+            'email_contacto': self.email_contacto,
+            'activo': self.activo
+        }
