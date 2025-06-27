b7460a74cc6484bf1603f161b3f11b1d92568194
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-06-27 19:49:46 -0300

    main app created

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from models.db import db
+from config.config import DATABASE_CONNECTION_URI
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    from models.musica import Musica
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
